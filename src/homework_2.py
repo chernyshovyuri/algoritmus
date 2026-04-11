f28ad396947baf9e4a7fb56fbd9c17b45bda8185
Yrii Chernyshov <c300@list.ru>
2026-04-11 21:00:26 +0300

    feat: Добавил фибоначи

--- src/homework_2.py
+++ src/homework_2.py
@@ -13,6 +13,25 @@ def factorial(n: int) -> int | None:
 
     return result
 
+def fibonaci(n: int | float)-> list[int|float] | None:
+    if not isinstance(n, (int, float)):
+        raise TypeError()
+
+    if n <= 0:
+        return None
+    if n == 1:
+        return [0]
+
+    fib_list = [0, 1]
+
+    while len(fib_list) < n:
+        fib_list.append(fib_list[-1] + fib_list[-2])
+
+    return fib_list
+
+
+
+
 
 
 
--- testes/test_homework_2.py
+++ testes/test_homework_2.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.homework_2 import factorial
+from src.homework_2 import factorial, fibonaci
 
 
 def test_incorrect_type():
@@ -17,3 +17,30 @@ def test_incorrect_type():
 
 def test_factorial(n, expected):
     assert factorial(n) == expected
+
+
+def test_incorrect_type():
+    with pytest.raises(TypeError):
+        fibonaci('f')
+
+def test_n_equal_to_zero():
+    assert not fibonaci(0)
+
+def test_n_less_zero():
+    assert not fibonaci(-1)
+
+def test_n_equal_to_one():
+    assert fibonaci(1) == [0], "result [0]"
+
+@pytest.mark.parametrize('n, expected',[
+    (5, [0,1,1,2,3]),
+    (0, None),
+    (1, [0]),
+    (-1, None),
+    (20, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]),
+    (15, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377])
+
+])
+
+def test_fibonaci(n, expected):
+    assert fibonaci(n) == expected
