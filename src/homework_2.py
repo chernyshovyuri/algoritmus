c11e41fa327a8fd643f8509e40f031a2dd8fcafd
Yrii Chernyshov <c300@list.ru>
2026-04-12 15:04:26 +0300

    feat: Добавил 5 задачу

--- src/homework_2.py
+++ src/homework_2.py
@@ -1,4 +1,4 @@
-
+import csv
 
 def factorial(n: int) -> int | None:
     if not isinstance(n, int):
@@ -66,6 +66,67 @@ def palindrome(n: int) -> bool | None:
         return True
 
     return False
+
+# Проблема:
+# Нужна программа котора будет обрабатывать посещаемость сайта. Необходимо пердоставить информацию о мах коичестве
+# посещаемости и минимальном кол. посещаемости в разрезе дней. Необходимо сделать срез по 3 дням, 3 дня с самым
+# мах посещением и з дня с минимальным посещением.
+
+# выходные данные:
+# Электронные таблицы где указано дата и количество посещений на эту дату. Четкая и понятная статистика.
+
+# Входные данные:
+#  Список дат с указанием посещений сайта
+
+# Ограничения:
+# Память 52 мб Скорость 5ск.
+
+# Тестирование.
+# Позитивные, негативные, пограничные.
+
+
+
+def reading_file(path: str, date_tuples: list) ->list:
+    if not isinstance(date_tuples, list):
+        raise TypeError()
+
+
+    with open(path, 'r') as file:
+        for line in file:
+            data_list = line.strip()
+            if data_list:
+                date, count = data_list.split(',')
+                date_tuples.append((date, int(count)))
+
+    return date_tuples
+
+date_vlues = []
+reading_file("date_visitors.txt", date_vlues)
+
+
+
+def search_for_max_and_min_values(date:list)-> list | None:
+
+    if not date:
+        return None
+
+    max_value = date[0]
+    min_value = date[0]
+
+    for i in range(1, len(date)):
+        if date[i][1] > max_value[1]:
+            max_value = date[i]
+        if date[i][1] < min_value[1]:
+            min_value = date[i]
+
+
+
+
+
+
+
+
+
 
 
 
